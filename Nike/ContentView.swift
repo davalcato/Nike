6ba00451fb6b7c970ad9c5a3b58d6165fe988501
davalcato <davalcato@yahoo.com>
2020-07-12 16:57:00 -0700

    first commit

---
+++ Nike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Nike
+//
+//  Created by Daval Cato on 7/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nike/NikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeApp.swift
+//  Nike
+//
+//  Created by Daval Cato on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct NikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
