c4c8e54d6f448b8f1a480c7ecc83eeb876c7dfd7
davalcato <davalcato@yahoo.com>
2020-07-19 15:00:28 -0700

    added the @State var selectedIndex = 0 to the struct MainView : View on the ContentView

--- Nike/ContentView.swift
+++ Nike/ContentView.swift
@@ -149,6 +149,9 @@ struct Home : View {
                     .foregroundColor(.black)
             }
             .padding(.top,25)
+            .padding(.horizontal)
+            
+            
             // Menu...
             
             ScrollView(.horizontal, showsIndicators: false) {
@@ -307,102 +310,146 @@ var topMenu = ["Shoes","Clothing","By Sports","By Brand","By Price"]
 struct MainView : View {
     
     @State var index = 0
+    @State var show = false
+    @State var selectedIndex = 0
+    
     
     var body: some View {
         
-        VStack(spacing: 0){
+        ZStack{
             
-            // This is where tab view with tabs will be
-            
-            // Here you will change tabs based on index...
-            
-            Home()
-            
-            
-            // More tab Views here...
-            HStack(spacing: 0){
+            VStack(spacing: 0){
                 
-                Button {
+                // This is where tab view with tabs will be
+                
+                // Here you will change tabs based on index...
+                
+                Home()
+                
+                // More tab Views here...
+                HStack(spacing: 0){
                     
-                    // The button animating here...
-                    
-                    withAnimation{
+                    Button {
                         
-                        index = 0
+                        // The button animating here...
+                        
+                        withAnimation{
+                            
+                            index = 0
+                        }
+                        
+                    } label: {
+                        
+                        HStack(spacing: 8){
+                            
+                            Image(systemName: "house.fill")
+                                .foregroundColor(index == 0 ? .white : Color.black.opacity(0.35))
+                                .padding(10)
+                                .background(index == 0 ? Color.black : Color.clear)
+                                .cornerRadius(8)
+                            
+                            Text(index == 0 ? "Home" : "")
+                                .foregroundColor(.black)
+                        }
                     }
                     
-                } label: {
+                    Spacer(minLength: 0)
                     
-                    HStack(spacing: 8){
+                    Button {
                         
-                        Image(systemName: "house.fill")
-                            .foregroundColor(index == 0 ? .white : Color.black.opacity(0.35))
-                            .padding(10)
-                            .background(index == 0 ? Color.black : Color.clear)
-                            .cornerRadius(8)
+                        // The button animating here...
                         
-                        Text(index == 0 ? "Home" : "")
-                            .foregroundColor(.black)
+                        withAnimation{
+                            
+                            index = 1
+                        }
+                        
+                    } label: {
+                        
+                        HStack(spacing: 8){
+                            
+                            Image(systemName: "suit.heart")
+                                .foregroundColor(index == 1 ? .white : Color.black.opacity(0.35))
+                                .padding(10)
+                                .background(index == 1 ? Color.black : Color.clear)
+                                .cornerRadius(8)
+                            
+                            Text(index == 1 ? "Loved" : "")
+                                .foregroundColor(.black)
+                        }
                     }
+                    
+                    Spacer(minLength: 0)
+                    
+                    Button {
+                        
+                        // The button animating here...
+                        
+                        withAnimation{
+                            
+                            index = 2
+                        }
+                        
+                    } label: {
+                        
+                        HStack(spacing: 8){
+                            
+                            Image(systemName: "person.fill")
+                                .foregroundColor(index == 2 ? .white : Color.black.opacity(0.35))
+                                .padding(10)
+                                .background(index == 2 ? Color.black : Color.clear)
+                                .cornerRadius(8)
+                            
+                            Text(index == 2 ? "Account" : "")
+                                .foregroundColor(.black)
+                        }
+                    }
+                    
                 }
+                .padding(.top)
+                .padding(.horizontal, 25)
+                .padding(.bottom,UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                // The reason is because all edges are ignored...
                 
-                Spacer(minLength: 0)
+            }
+            
+            // Here is the hero view...
+            
+            if show{
                 
-                Button {
+                VStack{
                     
-                    // The button animating here...
-                    
-                    withAnimation{
+                    HStack{
                         
-                        index = 1
+                        Button(action: {}) {
+                            
+                            Image(systemName: "suit.heart")
+                                .font(.system(size: 22))
+                                .foregroundColor(.white)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                            // Here we close hero view...
+                            withAnimation(.spring()){
+                                
+                                show.toggle()
+                            }
+                            
+                        }) {
+                            
+                            Image(systemName: "xmark")
+                                .font(.system(size: 22))
+                                .foregroundColor(.white)
+                        }
+                        
                     }
-                    
-                } label: {
-                    
-                    HStack(spacing: 8){
-                        
-                        Image(systemName: "suit.heart")
-                            .foregroundColor(index == 1 ? .white : Color.black.opacity(0.35))
-                            .padding(10)
-                            .background(index == 1 ? Color.black : Color.clear)
-                            .cornerRadius(8)
-                        
-                        Text(index == 1 ? "Loved" : "")
-                            .foregroundColor(.black)
-                    }
-                }
-                
-                Spacer(minLength: 0)
-                
-                Button {
-                    
-                    // The button animating here...
-                    
-                    withAnimation{
-                        
-                        index = 2
-                    }
-                    
-                } label: {
-                    
-                    HStack(spacing: 8){
-                        
-                        Image(systemName: "person.fill")
-                            .foregroundColor(index == 2 ? .white : Color.black.opacity(0.35))
-                            .padding(10)
-                            .background(index == 2 ? Color.black : Color.clear)
-                            .cornerRadius(8)
-                        
-                        Text(index == 2 ? "Account" : "")
-                            .foregroundColor(.black)
-                    }
+                    Image("p\(selectedIndex)")
                 }
                 
             }
-            .padding(.top)
-            .padding(.horizontal, 25)
-            .padding(.bottom,UIApplication.shared.windows.first?.safeAreaInsets.bottom)
-            // The reason is because all edges are ignored...
             
         }
         .edgesIgnoringSafeArea(.all)
