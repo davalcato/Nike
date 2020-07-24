9029ab37a0a56a92b87234897ea3b14cce83df85
davalcato <davalcato@yahoo.com>
2020-07-24 14:41:15 -0700

    added the Login and SignUp slider animation to the LogInView

--- Nike/LogInView.swift
+++ Nike/LogInView.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 struct LogInView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Home()
+        
     }
 }
+        
+        // For Smaller Size iPhones...
+                  
+//                  VStack{
+//
+//                      if UIScreen.main.bounds.height < 750{
+//
+//                          ScrollView(.vertical, showsIndicators: false) {
+//
+//                              Home()
+//                          }
+//                      }
+//                      else{
+//
+//                          Home()
+//                      }
+//                  }
+//                  .padding(.vertical)
+//              }
+//          }
 
 struct LogInView_Previews: PreviewProvider {
     static var previews: some View {
@@ -22,21 +44,26 @@ struct LogInView_Previews: PreviewProvider {
 struct Home : View {
     
     @State var index = 0
+    @Namespace var name
     
     var body: some View{
         
         VStack{
             
-            Image("Logo1")
+            Image("logo1")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 70, height: 70)
+                .frame(width: 70, height: 70.0)
                 .clipShape(Circle())
             
-            HStack{
+            HStack(spacing: 0){
                 
                 Button(action: {
                     
+                    withAnimation(.spring()){
+                        
+                        index = 0
+                    }
                     
                 }) {
                     
@@ -47,13 +74,68 @@ struct Home : View {
                             .fontWeight(.bold)
                             .foregroundColor(index == 0 ? .black : .gray)
                         
-                        Capsule()
-                            .fill(Color("Color"))
-                            .frame(height: 4)
+                        ZStack{
+                            
+                            Capsule()
+                                
+                                // Slide animation goes here...
+                                
+                                
+                                .fill(Color.black.opacity(0.04))
+                                .frame(height: 4)
+                            
+                            if index == 0{
+                                
+                                Capsule()
+                                    .fill(Color("Color2"))
+                                    .frame(height: 4)
+                                    .matchedGeometryEffect(id: "Tab", in: name)
+                            }
+                        }
+                    }
+                }
+                
+                Button(action: {
+                    
+                    withAnimation(.spring()){
+                        
+                        index = 1
+                    }
+                    
+                }) {
+                    
+                    VStack{
+                        
+                        Text("Sign Up")
+                            .font(.system(size:20))
+                            .fontWeight(.bold)
+                            .foregroundColor(index == 1 ? .black : .gray)
+                        
+                        ZStack{
+                            
+                            Capsule()
+                                
+                                // Slide animation goes here...
+                                
+                                
+                                .fill(Color.black.opacity(0.04))
+                                .frame(height: 4)
+                            
+                            if index == 1{
+                                
+                                Capsule()
+                                    .fill(Color("Color2"))
+                                    .frame(height: 4)
+                                    .matchedGeometryEffect(id: "Tab", in: name)
+                            }
+                        }
                         
                     }
                 }
             }
+            .padding(.top,30)
+            
+            Spacer()
         }
     }
 }
