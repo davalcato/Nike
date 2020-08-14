43ee481b4b1334241c0fb76d9f4213e52aebc993
davalcato <davalcato@yahoo.com>
2020-08-14 13:19:41 -0700

    comment

--- Nike/NikeApp.swift
+++ Nike/NikeApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct NikeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LogInView()
+            
+            
         }
     }
 }
